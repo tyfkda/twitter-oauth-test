b5629a8d8b6d53c4d537a124f241c0d610420192
tyfkda <tyfkda@gmail.com>
2014-11-12 15:35:56 +0900

    oauthの代わりにtwitter_oauthを使うよう変更

--- app.rb
+++ app.rb
@@ -1,10 +1,16 @@
 require 'sinatra'
-require 'oauth'
+require 'twitter_oauth'
 
-def oauth_consumer
+enable :sessions
+
+before do
   key = ENV['CONSUMER_KEY']
   secret = ENV['CONSUMER_SECRET']
-  OAuth::Consumer.new(key, secret, :site => 'https://api.twitter.com')
+  @twitter = TwitterOAuth::Client.new(
+      :consumer_key => key,
+      :consumer_secret => secret,
+      :token => session[:access_token],
+      :secret => session[:secret_token])
 end
 
 get '/' do
@@ -19,29 +25,24 @@ end
 
 get '/request_token' do
   callback_url = "#{base_url}/access_token"
-  request_token = oauth_consumer.get_request_token(:oauth_callback => callback_url)
+  request_token = @twitter.request_token(:oauth_callback => callback_url)
   session[:request_token] = request_token.token
   session[:request_token_secret] = request_token.secret
   redirect request_token.authorize_url
 end
 
 get '/access_token' do
-  request_token = OAuth::RequestToken.new(
-      oauth_consumer, session[:request_token], session[:request_token_secret])
-
   begin
-    @access_token = request_token.get_access_token(
-      {},
-      :oauth_token => params[:oauth_token],
-      :oauth_verifier => params[:oauth_verifier])
+    @access_token = @twitter.authorize(session[:request_token], session[:request_token_secret],
+                                       :oauth_verifier => params[:oauth_verifier])
   rescue OAuth::Unauthorized => @exception
     return erb :authorize_fail
   end
 
   session[:access_token] = @access_token.token
   session[:access_token_secret] = @access_token.secret
-  session[:user_id] = @access_token.params['user_id']
-  session[:screen_name] = @access_token.params['screen_name']
+  session[:user_id] = @twitter.info['user_id']
+  session[:screen_name] = @twitter.info['screen_name']
 
   erb :authorize_success
 end
